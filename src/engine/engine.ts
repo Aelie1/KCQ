63c14cf9fe0fa71f16cd94fed50d2719f2af4b90
Aelie <arieswind@gmail.com>
2026-09-04 14:04:13 -0400

    add launch configs for vscode

--- src/engine/engine.ts
+++ src/engine/engine.ts
@@ -1,11 +1,9 @@
 import type { GameState } from "./types";
 
-function createGameState(): GameState {
+export function createGameState(): GameState {
   return {
     metadata: {round:1,step:1,phase:"player"},
     characters: [],
     enemies: []
   };
 }
-
-
---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createGameState } from "./engine/engine";
+
+const state = createGameState();
+
+console.dir(state, { depth: null });
