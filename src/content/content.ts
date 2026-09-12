a89e02b9514cfc84c4a11324a09a7e9a1164b745
Aelie <arieswind@gmail.com>
2026-09-12 19:18:22 -0400

    guess i should move to loading plains_2

--- src/content/content.ts
+++ src/content/content.ts
@@ -1,6 +1,7 @@
 import { EncounterDef } from "../engine/itypes";
-import { plains_1 } from "./skunk/encounters";
+import { plains_1, plains_2 } from "./skunk/encounters";
 
 export const encounterList: EncounterDef[] = [
-    plains_1
+    plains_1,
+    plains_2
 ];
